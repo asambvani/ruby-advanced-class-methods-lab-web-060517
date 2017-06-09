a1a00d4eacd9fd3d4468d3082458f882f94a5af3
github username <alexsambvani@gmail.com>
2017-06-09 00:35:08 -0400

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Song
   attr_accessor :name, :artist_name
   @@all = []
@@ -8,6 +10,81 @@ class Song
 
   def save
     self.class.all << self
+  end
+
+  def Song.create
+
+    song = Song.new
+    song.save
+    return song
+
+  end
+
+  def Song.new_by_name(name)
+    song = Song.new
+    song.name = name
+    return song
+  end
+
+  def Song.create_by_name(name)
+    song = Song.new
+    song.name = name
+    song.save
+    return song
+  end
+
+  def Song.find_by_name(name)
+    return @@all.detect do |song|
+      song.name == name
+    end
+  end
+
+  def Song.find_or_create_by_name(name)
+
+    detected_song = @@all.detect do |song|
+      song.name == name
+    end
+    if detected_song == nil
+      return Song.create_by_name(name)
+    else
+      return detected_song
+    end
+  end
+
+  def Song.alphabetical
+
+    return @@all.sort_by do |song|
+      song.name
+    end
+
+  end
+
+  def Song.new_from_filename(file_name)
+
+    song_info = file_name.split(" - ")
+    song_info[1] = song_info[1].split(".")
+    song_info.flatten!
+    song = Song.new_by_name(song_info[1])
+    song.artist_name = song_info[0]
+    return song
+
+  end
+
+  def Song.create_from_filename(file_name)
+
+    song_info = file_name.split(" - ")
+    song_info[1] = song_info[1].split(".")
+    song_info.flatten!
+    song = Song.create_by_name(song_info[1])
+    song.artist_name = song_info[0]
+    return song
+
+  end
+
+  def Song.destroy_all
+
+    @@all.clear
+
   end
 
 end
